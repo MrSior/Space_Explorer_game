d73de8c4b10c000b421070e4f42950cde7cb4d67
MrSior <SemionChubenko@gmail.com>
2022-02-02 17:35:23 +0300

    added files

---
+++ cmake-build-debug/CMakeFiles/Space_Explorer_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Space_Explorer_game.dir/main.cpp.o"
+  "Space_Explorer_game"
+  "Space_Explorer_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Space_Explorer_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
